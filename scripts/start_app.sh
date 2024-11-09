1b3e9e21950d9105554d6af7db3818985f6e4dff
pochovillar <m.villar.barrios@gmail.com>
2024-11-09 15:35:22 -0700

    updated ip address

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/Resume/resume/settings.py
+sed -i 's/\[]/\["18.226.186.222"]/' /home/ubuntu/Resume/resume/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
