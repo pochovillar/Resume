9c364d094aca4c37c9686c157a2197b399093b5f
pochovillar <m.villar.barrios@gmail.com>
2024-11-09 14:25:41 -0700

    jdkfshsda

--- resume/settings.py
+++ resume/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--mi$m$8i_*e6p*i@883rnj6t!2#%yw$$+lev5jh+@hp^fttl3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.12.151.117']
+ALLOWED_HOSTS = ['18.226.186.222']
 
 
 
