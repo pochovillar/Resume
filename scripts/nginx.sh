a01d8011d5f26b0efa3b0cec298a9ae02e1e0203
pochovillar <m.villar.barrios@gmail.com>
2024-11-08 23:22:29 -0700

    nginx.sh changes

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/bash
 
+# Reload systemd to apply changes in the service configuration
 sudo systemctl daemon-reload
+
+# Remove default site link if it exists
 sudo rm -f /etc/nginx/sites-enabled/default
 
+# Ensure no existing symbolic link for the 'resume' site
+if [ -L /etc/nginx/sites-enabled/resume ]; then
+    sudo rm /etc/nginx/sites-enabled/resume
+fi
+
+# Copy the Nginx configuration file for the resume site to sites-available
 sudo cp /home/ubuntu/Resume/nginx/nginx.conf /etc/nginx/sites-available/resume
+
+# Create a symbolic link in sites-enabled
 sudo ln -s /etc/nginx/sites-available/resume /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
+
+# Add www-data user to the ubuntu group
 sudo gpasswd -a www-data ubuntu
+
+# Test the Nginx configuration
+sudo nginx -t
+
+# Restart Nginx to apply changes
 sudo systemctl restart nginx
+  
